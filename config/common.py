a574bdcc1086b627b8dcd39cfc743e89d2316f98
elijahanderson96 <elijahanderson96@gmail.com>
2022-10-28 15:43:47 -0700

    experimenting with building market wide models

--- config/common.py
+++ config/common.py
@@ -2,10 +2,11 @@ from config.configs import *
 import pandas as pd
 from helpers.helpers import parse_sql_file
 QUERIES = parse_sql_file(full_path='/mnt/c/Users/Elijah/PycharmProjects/Bagels/sql/queries.sql')
+SYMBOLS=pd.read_sql('''SELECT symbol FROM (SELECT symbol,
+count(symbol)
+FROM market.fundamental_valuations
+WHERE subkey='ttm'
+group by symbol) q
+WHERE q.count > 5
+ORDER BY symbol ASC;''',con=POSTGRES_URL)['symbol'].to_list()
 
-SYMBOLS=pd.read_sql('''SELECT *
-    FROM (SELECT DISTINCT symbol
-          FROM market.fundamental_valuations
-         ) t
-    ORDER BY random()
-    LIMIT 10;''',con=POSTGRES_URL)['symbol'].to_list()
