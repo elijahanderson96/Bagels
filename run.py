6c9d80d8a5ba5a94d8f070d15764a5dacc42b396
elijahanderson96 <elijahanderson96@gmail.com>
2023-01-04 11:20:39 -0800

    pretty messy commit but starting to incorporate random forest approach

--- run.py
+++ run.py
@@ -18,13 +18,18 @@ if __name__ == '__main__':
     DataGetter.update_data()
     prices = Prices()
     prices.update_db()
-    for company_size, symbols in sym_mkcap_mappings.items():
-        symbols = [symbol for symbol in symbols if symbol not in EXCLUDE_LIST]
-        try:
-            model = models.SectorModel(sector=symbols, model_type=company_size)
-            model.train(validate=True, interpolate_data=False, interpolate_labels=False)
-            model.predict()
-            model.save()
-        except:
-            print(print_exc())
-            print(f'Likely missing test data or labels')
+    symbols = pd.Series([symbol for symbol in SYMBOLS if symbol not in EXCLUDE_LIST])
+    model = models.SectorModel(sector=symbols, model_type='RANDOM')
+    model.train(validate=False, interpolate_data=False, interpolate_labels=False)
+    model.predict()
+    #for company_size, symbols in sym_mkcap_mappings.items():
+    #    symbols = [symbol for symbol in symbols if symbol not in EXCLUDE_LIST]
+    #    try:
+    #        model = models.SectorModel(sector=symbols, model_type=company_size)
+    #        model.train(validate=False, interpolate_data=False, interpolate_labels=False)
+    #        model.predict()
+    #        input('break')
+            #model.save()
+    #    except:
+    #        print(print_exc())
+    #        print(f'Likely missing test data or labels')
